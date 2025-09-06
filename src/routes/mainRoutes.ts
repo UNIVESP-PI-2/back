cb2c2765d149db4e3e87f8dad3d347b85ecac493
CaioSimioni <caiors.contato@gmail.com>
2025-09-06 05:12:43 -0700

    ci: implementando pipeline de testes e deploy

--- src/routes/mainRoutes.ts
+++ src/routes/mainRoutes.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import {login, responder, teste, testeLogado, pesquisar } from '../controllers/mainController'
 import { authMiddleware } from '../middlewares/auth';
 
-
 const router = express.Router();
 
 router.route('/teste').get(teste);
@@ -11,6 +10,4 @@ router.route('/listartudo').get(authMiddleware,testeLogado as any);
 router.route('/pesquisar').get(authMiddleware, pesquisar as any);
 router.route('/responder').post(responder as any);
 
-
 export {router as mainRouter};
-
